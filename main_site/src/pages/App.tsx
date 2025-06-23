6fee12044afd8263576a4151b31d948841108714
Andrew <andym.1098@gmail.com>
2025-06-23 00:16:52 -0400

    Is now live online

--- main_site/src/pages/App.tsx
+++ main_site/src/pages/App.tsx
@@ -1,18 +1,16 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import Resume from "./Resume";
 import Layout from "./layout";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="resume" element={<Resume />} />
-        </Route>
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="resume" element={<Resume />} />
+      </Route>
+    </Routes>
   );
 }
 
