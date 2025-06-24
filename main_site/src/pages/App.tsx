d209fc827b4d1f6768164ae48587699a0f170ea3
Andrew <andym.1098@gmail.com>
2025-06-24 14:57:50 -0400

    Added NavBar (#6)

--- main_site/src/pages/App.tsx
+++ main_site/src/pages/App.tsx
@@ -2,6 +2,8 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import Resume from "./Resume";
 import Layout from "./layout";
+import ContactMe from "./ContactMe";
+
 
 function App() {
   return (
@@ -9,6 +11,7 @@ function App() {
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
         <Route path="resume" element={<Resume />} />
+        <Route path="contact" element={<ContactMe/>}/>
       </Route>
     </Routes>
   );
