49669abbc33b9f9cb9c52b0513fab857b26fae00
Andrew <andym.1098@gmail.com>
2025-06-26 21:04:03 -0400

    Added in basic ContactMe banner

--- main_site/src/components/ContactMe/ContactMeBanner.tsx
+++ main_site/src/components/ContactMe/ContactMeBanner.tsx
@@ -4,7 +4,16 @@ function ContactMeBanner(){
 
     return (
         <>
-            <p>Banner</p>
+            <div className="container">
+                
+                <div className="d-flex">
+                    <div className="h1 p-2 flex-fill">Wanna link?</div>
+                    <div className="p-2 flex-fill">Checkout the below links, or shoot me an email!</div>
+                </div>
+                
+                <p>Test</p>
+
+            </div>
         </>
 
     );
