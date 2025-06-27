9a54d9f89b6a973ad9a33c360bf60a313144a09d
Andrew <andym.1098@gmail.com>
2025-06-26 21:13:05 -0400

    Removed junk from banner

--- main_site/src/components/ContactMe/ContactMeBanner.tsx
+++ main_site/src/components/ContactMe/ContactMeBanner.tsx
@@ -10,12 +10,8 @@ function ContactMeBanner(){
                     <div className="h1 p-2 flex-fill">Wanna link?</div>
                     <div className="p-2 flex-fill">Checkout the below links, or shoot me an email!</div>
                 </div>
-                
-                <p>Test</p>
-
             </div>
         </>
-
     );
 
 }
