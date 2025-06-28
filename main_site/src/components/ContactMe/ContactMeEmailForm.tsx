193b13d5e721ea766c1521ceeed8bb57ff37722d
Andrew <andym.1098@gmail.com>
2025-06-27 22:42:49 -0400

    Added in email form

--- main_site/src/components/ContactMe/ContactMeEmailForm.tsx
+++ main_site/src/components/ContactMe/ContactMeEmailForm.tsx
@@ -1,35 +1,69 @@
 
-function ContactMeEmailForm(){
+interface FormInfoFieldProp{
+    text : string,
+    field_id : string,
+    field_type?: string // Defaults to "text"
+}
 
+function FormInfoField({text, field_id, field_type = "text"}: FormInfoFieldProp){
     return(
-
         <>
-            <div>
-                <h1>Email Form</h1>
-                <p>
-                    Fill out the below fields, with a short message!
-                    I will reply as soon as I can!
-                </p>
-                <div> {/* Form components*/}
-                    <p>Your email</p>
-                    
-                    <p>Name</p>
-                    <p>Phone Number (optional)</p>
-                    
-                    <p>Header (Possibly)? (Would need backend I assume)</p>
-                    <p>Message Box</p>
-                    
-
-                    git rebase testing
-
-
-                    <p>Later on perhaps, but add a "are you a robot" thing</p>
-                    <p>Send button</p>
-                </div>
+            <div className="mb-3">
+                <label className="form-label">{text}</label>
+                <input type={field_type} className="form-control" id={field_id}/>
             </div>
         </>
-    );
+    )
+
+}
+
+function EmailForm() {
+  return (
+    <>
+        <form>
+            {/* Misc Fields */}
+            <FormInfoField text="Name" field_id="ContactMeEmailFormName" />
+            <FormInfoField text="Email" field_id="ContactMeEmailFormEmail" field_type="email" />
+            <FormInfoField text="Phone Number (Optional)" field_id="ContactMeEmailFormPhone"/>
+            <FormInfoField text="Linkedin Profile" field_id="ContactMeEmailFormLinkedin" />
+            <FormInfoField text="Header" field_id="ContactMeEmailFormHeader" />
+
+            {/* Message Box */}
+            <div>
+                <label className="form-label">Message</label>
+                <div className="input-group">
+                    <textarea className="form-control" aria-label="With textarea" id="ContactMeEmailFormMessage" required></textarea>
+                </div>
+            </div>
+            
+            <button type="submit" className="mt-3 btn btn-primary">Submit</button>
+        
+        </form>
+
+
+    </>
+  );
+}
+
+function ContactMeEmailForm() {
+  return (
+    <>
+      <div>
+        <div>
+          <h1>Email Form</h1>
+          <p>
+            Fill out the below fields, with a short message! I will reply as
+            soon as I can!
+          </p>
+        </div>
+
+        <div>
+            <EmailForm />
+        </div>
+
+      </div>
+    </>
+  );
 }
 
 export default ContactMeEmailForm;
-
