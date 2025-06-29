6ec6ebb3759726698084072a45e0d3cc3b8dd505
Andrew <andym.1098@gmail.com>
2025-06-29 09:55:36 -0400

    Changing banner content layout

--- main_site/src/components/ContactMe/ContactMeBanner.tsx
+++ main_site/src/components/ContactMe/ContactMeBanner.tsx
@@ -4,12 +4,9 @@ function ContactMeBanner(){
 
     return (
         <>
-            <div className="container">
-                
-                <div className="d-flex">
-                    <div className="h1 p-2 flex-fill">Wanna link?</div>
-                    <div className="p-2 flex-fill">Checkout the below links, or shoot me an email!</div>
-                </div>
+            <div>
+                <h1>Wanna link?</h1>
+                <p>Checkout the below links, or shoot me an email!</p>
             </div>
         </>
     );
